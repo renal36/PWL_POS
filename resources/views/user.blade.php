a1fc6af6b666473a1f6be555706c7a9c10e9b170
Tyo Renaldi <renaldityo85@gmail.com>
2025-05-22 23:48:13 +0700

    Update user.blade.php dan tambah folder adminlte + layouts

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,19 +7,22 @@
     <h1>Data User dengan Level 2</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
             <th>Aksi</th>
         </tr>
         @foreach ($data as $data) 
         <tr>
             <td>{{ $data->user_id }}</td> 
             <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
+            <td>{{ $data->nama }}</td>-
             <td>{{ $data->level_id }}</td>
-            <td>
+            <td>{{ $data->level->level_kode }}</td>
+            <td>{{ $data->level->level_nama }}</td>
                 <a href="/user/ubah/{{ $data->user_id }}">Ubah</a> | 
                 <a href="/user/hapus/{{ $data->user_id }}">Hapus</a>
             </td>
